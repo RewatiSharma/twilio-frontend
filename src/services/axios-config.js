813ce4b66b4f82c488979d654b1881e5b5a1dbf8
RewatiSharma <rewatisharmaa@gmail.com>
2024-02-01 12:52:18 +0530

    axios url changed

--- src/services/axios-config.js
+++ src/services/axios-config.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-let baseURL = "http://localhost:3001";
+// let baseURL = "http://localhost:3001";
+let baseURL = "https://twilio-sms-hv3x.onrender.com";
 
 const publicReq = axios.create({ 
     baseURL 
